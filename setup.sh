07b53ce6fcd4baaaa004b0abbfc0f7c2a693f0bb
den9 <koji.hachiya@gmail.com>
2024-02-09 18:37:43 +0900

    add launcher to setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 sudo apt-get install python python-yaml python-setuptools python-pexpect python-requests python-twisted omxplayer fbi -y
 sudo python setup.py install
+sudo cp launcher/raspi-signage-startup.sh /usr/local/bin
+sudo sed -i '/^exit 0$/i /usr/local/bin/raspi-signage-startup.sh' /etc/rc.local
